00643c3b7c5045b8f08a82905f7ffdcf4eea7312
Oleh Lavryk <oleh.lavrik@gmail.com>
2022-11-18 16:41:09 +0200

    style: 💄 fixed formating

--- commitizen.config.js
+++ commitizen.config.js
@@ -1,9 +1,9 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const custom = require('@digitalroute/cz-conventional-changelog-for-jira/configurable')
+const custom = require("@digitalroute/cz-conventional-changelog-for-jira/configurable");
 
 module.exports = custom({
-	jiraPrefix: 'CI',
-	jiraLocation: 'post-description',
-	jiraPrepend: '[',
-	jiraAppend: ']',
-})
+  jiraPrefix: "CI",
+  jiraLocation: "post-description",
+  jiraPrepend: "[",
+  jiraAppend: "]",
+});
--- commitlint.config.js
+++ commitlint.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-	extends: ['@commitlint/config-conventional'],
-	ignores: [(message) => /^chore\(release\):/.test(message)],
-}
+  extends: ["@commitlint/config-conventional"],
+  ignores: [(message) => /^chore\(release\):/.test(message)],
+};
