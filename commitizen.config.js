f2d5cab906abd3cd634063fff7f45db74db5a576
Oleh Lavryk <oleh.lavrik@gmail.com>
2022-11-18 08:42:19 +0200

    ci: 🎡 add commitizen

---
+++ commitizen.config.js
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const custom = require('@digitalroute/cz-conventional-changelog-for-jira/configurable')
+
+module.exports = custom({
+	jiraPrefix: 'CI',
+	jiraLocation: 'post-description',
+	jiraPrepend: '[',
+	jiraAppend: ']',
+})
