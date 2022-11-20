06201bab1576b9b0e3cd79ae0cdbf39ce93c481c
Oleh Lavryk <oleh.lavrik@gmail.com>
2022-11-20 14:32:37 +0200

    feat: 🎸 add new feature

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Test
         </a>
         <h1>React.js</h1>
         <p>React the best library</p>
