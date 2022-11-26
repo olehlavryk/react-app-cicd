a9d9915d2e5765ac5a1103d0fafaee0269331f58
Oleh Lavryk <oleh.lavrik@gmail.com>
2022-11-26 12:05:04 +0200

    refactor: 💡 discover about core

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -3,13 +3,22 @@ const github = require("@actions/github");
 
 try {
   // throw (new Error("Some error message"))
+
+  core.debug("debug message");
+  core.warning("warning message");
+  core.error("error message");
+
   const name = core.getInput("who-to-greed");
+  core.setSecret(name);
   console.log(`Hello ${name}`);
 
   const time = new Date();
   core.setOutput("time", time.toTimeString());
-
+  core.startGroup("Logging github object");
   console.log(JSON.stringify(github, null, "\t"));
+  core.endGroup()
+
+  core.exportVariable("HELLO", "hello")
 } catch (error) {
   core.setFailed(error.message);
 }
