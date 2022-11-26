615de9777fcfcd9d5deef82801691829e617c3b5
Oleh Lavryk <oleh.lavrik@gmail.com>
2022-11-26 16:35:10 +0200

    feat: 🎸 add create an issue action

---
+++ .github/actions/issue/index.js
@@ -0,0 +1,27 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+const run = async () => {
+  try {
+    const token = core.getInput("token");
+    const title = core.getInput("title");
+    const body = core.getInput("body");
+    const assignees = core.getInput("assignees");
+
+    const octokit = new github.GitHub(token);
+
+    const response = await octokit.rest.issues.create({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      title,
+      body,
+      assignees: assignees ? assignees.split("\n") : undefined,
+    });
+
+    core.setOutput("issue", JSON.stringify(response.data));
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+};
+
+run();
